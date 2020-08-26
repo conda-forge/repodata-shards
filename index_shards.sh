f1b97d94641d5c3182058fdb58d5f56b2b32524e
Matthew R Becker <becker.mr@gmail.com>
2020-08-26 13:16:31 -0500

    fix bugs in releases

--- index_shards.sh
+++ index_shards.sh
@@ -7,11 +7,16 @@ git config --global user.name 'conda-forge-daemon'
 git config --global pull.rebase true
 
 pushd ..
+
 git clone --depth=1 https://github.com/regro/repodata-tools.git
 pushd repodata-tools
 conda install -y -q --file requirements.txt
 pip install -e .
 popd
+
+git clone https://github.com/regro/releases.git
+releases_repo_path=`pwd`/releases
+
 popd
 
 pushd ${GITHUB_WORKSPACE}
@@ -19,6 +24,6 @@ pushd ${GITHUB_WORKSPACE}
 repo_url=https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 git remote set-url --push origin ${repo_url}
 
-sync-anaconda-data --rank=${RANK} --n-ranks=4
+sync-anaconda-data --rank=${RANK} --n-ranks=4 --releases-repo-path=${releases_repo_path}
 
 popd
