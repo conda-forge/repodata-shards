58bc7e0be12d5e399425ccd3f587835765b7af39
Matthew R. Becker <beckermr@users.noreply.github.com>
2022-03-05 06:53:11 -0600

    Update index_shards.sh

--- index_shards.sh
+++ index_shards.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-git config --global user.email 'pelson.pub+conda-forge@gmail.com'
-git config --global user.name 'conda-forge-admin'
-git config --global pull.rebase false
-
-pushd ..
-git clone --depth=1 https://github.com/conda-forge/repodata-tools.git
-pushd repodata-tools
-pip install -e .
-popd
-popd
-
 pushd ${GITHUB_WORKSPACE}
 
 repo_url=https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
